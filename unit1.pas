fb2bdbbe61918abffdc9401cd7e5f81411145ff4
rand0m_7 <erovlev2@yandex.ru>
2025-05-22 20:36:10 +0300

    Update interface | Add connect function

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,25 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  RS232Port;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    baudEdit: TComboBox;
+    disconnectButton: TButton;
+    connectButton: TButton;
+    console: TMemo;
+    COMPort: TRS232Port;
+    connectBox: TGroupBox;
+    connectPort: TLabeledEdit;
+    Label1: TLabel;
+    sendButton: TButton;
+    sendEdit: TLabeledEdit;
+    procedure connectButtonClick(Sender: TObject);
   private
 
   public
@@ -21,6 +36,29 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.connectButtonClick(Sender: TObject);
+begin
+  COMPort.Device:=connectPort.Text;
+                                                          
+  if baudEdit.Text='1200' then COMPort.BaudRate:=br001200;
+  if baudEdit.Text='2400' then COMPort.BaudRate:=br002400;
+  if baudEdit.Text='4800' then COMPort.BaudRate:=br004800;  
+  if baudEdit.Text='9600' then COMPort.BaudRate:=br009600; 
+  if baudEdit.Text='19200' then COMPort.BaudRate:=br019200;
+  if baudEdit.Text='38400' then COMPort.BaudRate:=br038400;
+  if baudEdit.Text='57600' then COMPort.BaudRate:=br057600;
+  if baudEdit.Text='115200' then COMPort.BaudRate:=br115200;
+
+  try
+    COMPort.Open;
+  except
+    on E: ERS232OpenError do
+      ShowMessage(E.PortName + ': ' + E.Message + '.');
+  end;
+end;
 
 end.
 
